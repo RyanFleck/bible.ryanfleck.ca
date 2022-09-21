6f8fcbe3372cb0d8ebb03ae6e4c02f81a4195def
Ryan Fleck <Ryan.Fleck@protonmail.com>
2022-09-21 10:14:02 -0600

    Work on landing page.

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,42 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import bibleSvg from "./assets/bible.svg";
+  import Counter from "./lib/Counter.svelte";
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <div class="title-container">
+    <img src={bibleSvg} class="logo svelte" alt="Svelte Logo" />
+    <h1 class="title">Holy Bible</h1>
   </div>
-  <h1>Vite + Svelte</h1>
 
   <div class="card">
     <Counter />
   </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
   .logo {
-    height: 6em;
-    padding: 1.5em;
+    height: 60px;
     will-change: filter;
+    padding: 0;
   }
   .logo:hover {
     filter: drop-shadow(0 0 2em #646cffaa);
   }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
+  .title {
+    padding: .2em;
+    font-size: 60px;
   }
-  .read-the-docs {
-    color: #888;
+  .title-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+  }
+  .title-container > img {
+    flex: 1;
+  }
+  .title-container > h1 {
+    flex-grow: 4;
   }
 </style>
