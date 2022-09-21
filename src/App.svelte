bd68d73386a46bda574cf9602220326766d2f8c6
Ryan Fleck <Ryan.Fleck@protonmail.com>
2022-09-21 10:28:39 -0600

    Handle simple input.

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,14 @@
 <script>
   import bibleSvg from "./assets/bible.svg";
   import Counter from "./lib/Counter.svelte";
+  import kjv from "./assets/kjv.json";
+
+  let scripture = "";
+
+  const handleInput = (e) => {
+    let text = e.target.value;
+    console.log(text);
+  };
 </script>
 
 <main>
@@ -8,10 +16,18 @@
     <img src={bibleSvg} class="logo svelte" alt="Svelte Logo" />
     <h1 class="title">Holy Bible</h1>
   </div>
-  <small>If you are reading this the entire King James Bible has been loaded into your RAM.</small>
+  <small
+    >If you are reading this the entire King James Bible has been loaded into
+    your RAM.</small
+  >
 
   <div class="card">
     <Counter />
+  </div>
+
+  <div class="scripture-search">
+    <input type="text" name="scripture" on:input={handleInput} />
+    <p><i>{scripture}</i></p>
   </div>
 </main>
 
@@ -25,7 +41,7 @@
     filter: drop-shadow(0 0 2em #646cffaa);
   }
   .title {
-    padding: .2em;
+    padding: 0.2em;
     font-size: 60px;
     margin: 0;
   }
