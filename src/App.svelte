4043fd9315983817b80902b4e0ce4bddd291cca5
Ryan Fleck <Ryan.Fleck@protonmail.com>
2022-09-21 11:03:45 -0600

    Split out numeric and text components in verse q

--- src/App.svelte
+++ src/App.svelte
@@ -2,12 +2,50 @@
   import bibleSvg from "./assets/bible.svg";
   import Counter from "./lib/Counter.svelte";
   import kjv from "./assets/kjv.json";
+  import books from "./assets/books.json";
+
+  // Build a list of all Bible book names.
+  let allBooks = [];
+  allBooks = allBooks.concat(books["New Testament"]);
+  allBooks = allBooks.concat(books["Old Testament"]);
+
+  // let list = "";
+  // allBooks.forEach((x) => (list = list.concat(`${x}\n`)));
+  // console.log(list);
 
   let scripture = "";
 
+  let book_number = 0;
+  let book_name = "";
+  let verse = "";
+
   const handleInput = (e) => {
     let text = e.target.value;
-    console.log(text);
+    let all_components = [].concat(text.match(/[a-zA-Z]+|[0-9\:\-]+/g));
+    let text_components = [];
+    let numeric_components = [];
+    if (all_components.length > 0) {
+      // Split into numeric and textual components.
+
+      for (let x = 0; x < all_components.length; x++) {
+        let content = all_components[x];
+        if (!isNaN(content[0])) {
+          numeric_components.push(content);
+        } else {
+          text_components.push(content);
+        }
+      }
+
+      if (!isNaN(text_components[0])) {
+        book_number = parseInt(text_components[0]);
+        text_components = text_components.shift();
+      } else {
+        book_number = 0;
+      }
+
+      console.log(text_components);
+      console.log(numeric_components);
+    }
   };
 </script>
 
@@ -21,13 +59,12 @@
     your RAM.</small
   >
 
-  <div class="card">
-    <Counter />
-  </div>
-
   <div class="scripture-search">
     <input type="text" name="scripture" on:input={handleInput} />
     <p><i>{scripture}</i></p>
+    <p>Book Number: {book_number}</p>
+    <p>Book Name: {book_name}</p>
+    <p>Verse: {verse}</p>
   </div>
 </main>
 
@@ -50,5 +87,9 @@
     flex-wrap: wrap;
     justify-content: center;
     align-items: center;
+  }
+  .scripture-search > input {
+    padding: 0.5em;
+    margin: 1em;
   }
 </style>
